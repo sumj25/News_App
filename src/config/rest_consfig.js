0d923429178e6c8e672a2ae6d4c2cd4416fe4874
Suman kumar <59159432+sumj25@users.noreply.github.com>
2021-05-18 19:16:06 +0530

    Update rest_consfig.js

--- src/config/rest_consfig.js
+++ src/config/rest_consfig.js
@@ -1,4 +1,4 @@
 export const articles_url = 'https://newsapi.org/v2/top-headlines';
 export const country_code = 'in';
 export const category = 'general';
-export const _api_key = '6d892cc1c2ca47f2a3930541273ff7c1';
+export const _api_key = 'xxxxxxx';
